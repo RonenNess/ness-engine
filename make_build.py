d6241ef80947932a19d499775161dc24a002445a
Ronen Ness <RonenNess@gmail.com>
2014-07-11 14:59:05 +0000

    working on making build script

--- make_build.py
+++ make_build.py
@@ -18,4 +18,23 @@ shutil.copyfile(os.path.join("libs", "README-SDL.txt"), "last_build/lib/README-S
 # copy the include files
 shutil.copytree("sdl/include", "last_build/include")
 
+# copy all the ness-engine header files and directories structure
+for root, dirs, files in os.walk("source"):
+
+    # remove the 'source/NessEngine' part from the dir
+    relative_path = root[len("source/NessEngine"):]
+
+    # if directory does not exist in destination, create it
+    FullDirPath = "last_build/include" + relative_path
+    if not os.path.exists(FullDirPath):
+        os.makedirs(FullDirPath)
+
+    # now loop over all files in dir and copy all header files
+    for file in files:
+        if file[-2:] != '.h':
+            continue
+        src = os.path.join(root, file)
+        dest = os.path.join(FullDirPath, file)
+        shutil.copyfile(src, dest)
+
 raw_input("Done!")
