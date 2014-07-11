a269594a455bcebecbc199233d26221c1bb9d8b2
Ronen Ness <RonenNess@gmail.com>
2014-07-11 14:24:52 +0000

    working on script to create build structure

--- make_build.py
+++ make_build.py
@@ -3,12 +3,19 @@ import os, shutil
 BUILD_TYPE = 'x86'
 PLATFORM = 'windows'
 
-# create/recreate the last_build dir
+# if already exist previous build, remove it
 if os.path.isdir("last_build"):
-    os.rmdir("last_build")
+    shutil.rmtree("last_build")
+
+# create the last-build dir
 os.mkdir("last_build")
-os.mkdir("last_build/lib")
 
+# copy the libs dir based on platform and build type, and also copy the readme files
+shutil.copytree(os.path.join("libs", PLATFORM, BUILD_TYPE), "last_build/lib")
+shutil.copyfile(os.path.join("libs", "README-NESSENGINE.txt"), "last_build/lib/README-NESSENGINE.txt")
+shutil.copyfile(os.path.join("libs", "README-SDL.txt"), "last_build/lib/README-SDL.txt")
 
+# copy the include files
+shutil.copytree("sdl/include", "last_build/include")
 
 raw_input("Done!")
