c89b83304c61cc572714d179748e31a1bc8fdb89
Ronen Ness <RonenNess@gmail.com>
2014-07-13 00:13:36 +0000

    started working on animators

---
+++ source/NessEngine/animators/animator_api.h
@@ -0,0 +1,28 @@
+/**
+* the API for animator object - object that attach to a renderable and "runs" on it every frame.
+* Author: Ronen Ness
+* Since: 07/1014
+*/
+
+#pragma once
+#include "../exports.h"
+#include "../renderable/renderable_api.h"
+
+class AnimatorsManager;
+
+namespace Ness
+{
+	// the animator class API
+	class AnimatorAPI
+	{
+	private:
+		AnimatorsManager* m_manager;
+	public:
+		
+		// set the animated object or add it to a list if this animator support multiply objects
+		NESSENGINE_API virtual void set_animated(RenderablePtr);
+
+		// animate this object. return false when you want to remove this animator from the animations manager
+		NESSENGINE_API virtual bool animate();
+	};
+};
