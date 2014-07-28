17c59034300b2b503b4efe84c12d3f3324d047ff
Ronen Ness <RonenNess@gmail.com>
2014-07-28 14:15:51 +0000

    added constructors to rectangle

--- examples/ness-engine/include/basic_types/rectangle.h
+++ examples/ness-engine/include/basic_types/rectangle.h
@@ -7,9 +7,26 @@
 #pragma once
 #include <SDL.h>
 #include "point.h"
+#include "size.h"
 #include "../exports.h"
 
 namespace Ness
 {
-	typedef SDL_Rect Rectangle;
+	class Rectangle : public SDL_Rect
+	{
+	public:
+		Rectangle()
+		{
+		}
+
+		Rectangle(int X, int Y, int W, int H)
+		{ 
+			x = X; y = Y; w = W; h = H;
+		}
+
+		Rectangle(const Pointi& pos, const Sizei& size)
+		{ 
+			x = pos.x; y = pos.y; w = size.x; h = size.y;
+		}
+	};
 };
