bc8b42cd9736c3e5f90861e3c051494dc541162e
Ronen Ness <RonenNess@gmail.com>
2014-11-09 23:58:58 +0000

    working on sprite character animator

--- source/NessEngine/animators/animator_sprite_character.h
+++ source/NessEngine/animators/animator_sprite_character.h
@@ -32,6 +32,7 @@
 #include "animator_api.h"
 #include "../renderer/renderer.h"
 #include "../renderable/entities/sprite.h"
+#include <math.h>
 
 namespace Ness
 {
@@ -42,14 +43,14 @@ namespace Ness
 		struct SpriteAnimationStep
 		{
 			Pointi position;		// the index of the animation step inside the spritesheet
-			float duration;			// for how long (in seconds) to wait on this step before moving to the next step
+			float duration;			// for how long to wait (in seconds) on this step before moving to the next step
 		};
 
 		// represent an entire animation sequence, composed of steps and optional texture
 		struct SpriteAnimationSequence
 		{
-			String change_texture;								// optional, if not empty string will change to new texture
-			Pointi texture_steps_count;							// optional, if not Pointi::ZERO will reset the number of steps in the texture spritesheet (use if you change texture)
+			String change_texture;								// optional, if not empty string will switch to the given texture
+			Pointi texture_steps_count;							// how many steps there are in total in the texture spritesheet
 			Containers::Vector<SpriteAnimationStep> steps;		// the steps of this animation sequence
 		};
 
@@ -62,14 +63,18 @@ namespace Ness
 		private:
 			SharedPtr<AnimatorSpritesheetAnimations>	m_animations;		// all the animation sequences (must be defined outside)
 			float										m_speed;			// animations speed factor
+			SpriteAnimationStep*						m_curr_step;		// current step in current animation
 			SpriteAnimationSequence*					m_curr_animation;	// current animation playing
 			float										m_step_in_anim;		// current step in current animation playing
+			String										m_default_anim;		// default animation to return to when completing cycle
+			bool										m_loop;				// should we loop on the current animation
+			float										m_time_to_wait;		// time to wait on current step
 
 		public:
 			// target - target sprite to animate
 			// animations - all the character animations
 			NESSENGINE_API AnimatorSpriteCharacter(const SpritePtr& target, const SharedPtr<AnimatorSpritesheetAnimations>& animations) :
-			TargetAnimatorAPI(target), m_animations(animations), m_speed(1.0f)
+			TargetAnimatorAPI(target), m_animations(animations), m_speed(1.0f), m_curr_step(nullptr), m_curr_animation(nullptr), m_step_in_anim(0.0f), m_loop(false), m_time_to_wait(0.0f)
 			{
 			}
 
@@ -77,10 +82,96 @@ namespace Ness
 			NESSENGINE_API inline void set_speed(float speed) {m_speed = speed;}
 			NESSENGINE_API inline float get_speed() {return m_speed;}
 
+			// set/get default animation
+			NESSENGINE_API inline void set_default_animation(const String& animName) {m_default_anim = animName;}
+			NESSENGINE_API inline const String& get_default_animation() {return m_default_anim;}
 
+			// start playing an animation
+			// animationName is the name of the animation to play
+			// if loop is true it will continue to play this animation in an endless loop until other animation is set
+			NESSENGINE_API void play_animation(const String& animationName, bool loop)
+			{
+				// set loop
+				m_loop = loop;
+
+				// set current animation
+				m_curr_animation = &((*m_animations)[animationName]);
+
+				// reset the new animation
+				reset_current_animation();
+			}
+
+			// reset the currently playing animation back to the starting point
+			NESSENGINE_API void reset_current_animation()
+			{
+				m_step_in_anim = 0.0f;
+				m_time_to_wait = 0.0f;
+				m_curr_step = nullptr;
+			}
+
+			// end the currently playing animation.
+			// if animation set to loop it will be equevilent to reset_current_animation().
+			// if not in loop then if there is default animation it will switch to default, else it will
+			// simple get "stuck" on the last step of the animation.
+			NESSENGINE_API void end_current_animation()
+			{
+				// if animation is in loop, reset the current animation
+				if (m_loop)
+				{
+					reset_current_animation();
+				}
+				// if animation is not in loop, either return to default or set to last step
+				else
+				{
+					if (m_default_anim.length() > 0)
+					{
+						play_animation(m_default_anim, true);
+					}
+					else
+					{
+						m_step_in_anim = (float)(m_curr_animation->steps.size() - 1);
+					}
+				}
+			}
+
+			// do the animation
 			NESSENGINE_API virtual void do_animation(Renderer* renderer)
 			{
-				
+				// if no animation is playing nothing to do
+				if (m_curr_animation == nullptr)
+					return;
+
+				// get current step index
+				unsigned int curr_step_int = (unsigned int)floorf(m_step_in_anim);
+
+				// check if animation ended
+				if (curr_step_int >= m_curr_animation->steps.size())
+				{
+					end_current_animation();
+				}
+				else
+				{
+					// increase current animation step
+					if (m_time_to_wait > 0.0f)
+					{
+						m_time_to_wait -= renderer->time_factor() * m_speed;
+					}
+					else
+					{
+						m_step_in_anim += renderer->time_factor() * m_speed;
+					}
+				}
+
+				// get current animation step data
+				SpriteAnimationStep* newStep = &m_curr_animation->steps[curr_step_int];
+
+				// check if we changed the current animation step
+				if (newStep != m_curr_step)
+				{
+					m_curr_step = newStep;
+					m_time_to_wait = m_curr_step->duration;
+					(ness_ptr_cast<Sprite>(m_target))->set_source_from_sprite_sheet(m_curr_step->position, m_curr_animation->texture_steps_count);
+				}
 			}
 		};
 
